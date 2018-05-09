f0d8d951b9f37ae107acce1a625826389eed19f0
William Kenji Kiplinger <wkkiplinger@cpp.edu>
2018-05-08 20:56:59 -0700

    Health and Mana bar attached to Player and Scripts.
    Added:
    -Player Script
    -Stat Script
    -BarScript
    
    Added, but no code:
    -GameController
    -GameController Script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField]
+	private Stat health;
+	[SerializeField]
+	private Stat Mana;
+
+	private void Awake()
+	{
+		health.Initialize ();
+		Mana.Initialize ();
+	}
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Q))
+		{
+			health.CurrentVal -= 5;
+		}
+		if(Input.GetKeyDown(KeyCode.W))
+		{
+			health.CurrentVal += 5;
+		}
+
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			Mana.CurrentVal -= 1;
+		}
+		if(Input.GetKeyDown(KeyCode.S))
+		{
+			Mana.CurrentVal += 1;
+		}
+	}
+}
