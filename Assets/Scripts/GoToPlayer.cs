58229237bd761b3a2db7729d0c978c4150382920
jcarreracpp <jcarrera@cpp.edu>
2018-05-08 19:59:03 -0700

    Added a baked navmesh, enemy object, and navmesh agent to the enemy object.

---
+++ Assets/Scripts/GoToPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToPlayer : MonoBehaviour
+{
+    Transform player;               // Reference to the player's position.
+    UnityEngine.AI.NavMeshAgent nav;               // Reference to the nav mesh agent.
+
+
+    void Awake()
+    {
+        // Set up the references.
+        player = GameObject.Find("Player").transform;
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+            nav.SetDestination(player.position);
+            nav.enabled = true;
+    }
+}
