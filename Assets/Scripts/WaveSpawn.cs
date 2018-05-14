98857478863e76884af150e3ef4e4710a56b1e5d
bemetcalf <bryc3m3tcalf@aol.com>
2018-05-13 20:54:58 -0700

    Models added, spawner changed
    added models:
    farmer pitchfork
    farmer hoe
    farmer scythe
    knight sheild and sword
    
    added models to spawner
    
    changed spawner to spawn random units from selected faction

--- Assets/Scripts/WaveSpawn.cs
+++ Assets/Scripts/WaveSpawn.cs
@@ -6,51 +6,72 @@ public class WaveSpawn : MonoBehaviour {
     public GameObject enemy;
     public GameObject elf;
     public GameObject orc;
-    public GameObject farmer;
-    public GameObject knight;
-    public List<GameObject> enemies;
+    public GameObject farmerHoe;
+    public GameObject farmerPitchfork;
+    public GameObject farmerScythe;
+    public GameObject knightSheildSword;
+    
     public List <Transform> spawnPoints;
     bool started = false;
-    int i = 0;
+    int enemyCount = 0;
+    public int maxEnemyCount;
     int waveType;
+    public List<GameObject> enemies;
     
+
 
 
     // Use this for initialization
     void Start () {
-        waveType = Random.Range(0, 4);
+        waveType = Random.Range(0, 5);
         spawnPoints.Add(GameObject.Find("SpawnPoint1").transform);
         spawnPoints.Add(GameObject.Find("SpawnPoint2").transform);
         spawnPoints.Add(GameObject.Find("SpawnPoint3").transform);
 
-        enemies.Add(enemy);
-        enemies.Add(farmer);
-        enemies.Add(knight);
-        enemies.Add(elf);
-        enemies.Add(orc);
-       
         
+
+        switch (waveType)
+        {
+            case 1:
+                enemies.Add(farmerHoe);
+                enemies.Add(farmerPitchfork);
+                enemies.Add(farmerScythe);
+                break;
+            case 2:
+                enemies.Add(knightSheildSword);
+                break;
+            case 3:
+                enemies.Add(elf);
+                break;
+            case 4:
+                enemies.Add(orc);
+                break;
+            default:
+                enemies.Add(enemy);
+                break;
+
+        }
 
 
     }
 
     // Update is called once per frame
-    void Update() {
-        int chance = Random.Range(0,1000);
-        if (started && i < 20 && chance > 982)
+    void Update()
+    {
+        int chance = Random.Range(0, 1000);
+        if (started && enemyCount < maxEnemyCount && chance > 982)
         {
-            int index = Random.Range(0, spawnPoints.ToArray().Length);
-            Instantiate(enemies[waveType], spawnPoints[index].position, spawnPoints[index].rotation);
-            i++;
+            int enemyIndex = Random.Range(0, enemies.ToArray().Length);
+            int spawnPointIndex = Random.Range(0, spawnPoints.ToArray().Length);
+            Instantiate(enemies[enemyIndex], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+            enemyCount++;
         }
+
         if (Input.GetKeyDown(KeyCode.P))
         {
             started = true;
         }
+            
         
-
-
-
-
     }
 }
