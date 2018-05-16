6ed31692c0b47baa86282822a4d6ead05dafc8c4
bemetcalf <bryc3m3tcalf@aol.com>
2018-05-15 20:43:38 -0700

    enemy and spawn points added
    12 enemies currently put in
    
    21 new spawnpoins added to create better grouping
    
    enemies added to wave spawn script

--- Assets/Scripts/WaveSpawn.cs
+++ Assets/Scripts/WaveSpawn.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class WaveSpawn : MonoBehaviour {
     public GameObject enemy;
-    public GameObject elf;
-    public GameObject orc;
+    public GameObject elfMage;
+    public GameObject elfSage;
+    public GameObject elfBow;
+    public GameObject orcMelee;
+    public GameObject orcThrowing;
+    public GameObject orcBrute;
     public GameObject farmerHoe;
     public GameObject farmerPitchfork;
     public GameObject farmerScythe;
     public GameObject knightSheildSword;
-    
+    public GameObject knightLance;
+    public GameObject knightCrossbow;
+
     public List <Transform> spawnPoints;
     bool started = false;
     int enemyCount = 0;
@@ -27,9 +33,27 @@ public class WaveSpawn : MonoBehaviour {
         spawnPoints.Add(GameObject.Find("SpawnPoint1").transform);
         spawnPoints.Add(GameObject.Find("SpawnPoint2").transform);
         spawnPoints.Add(GameObject.Find("SpawnPoint3").transform);
-
-        
-
+        spawnPoints.Add(GameObject.Find("SpawnPoint4").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint5").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint6").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint7").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint8").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint9").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint10").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint11").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint12").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint13").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint14").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint15").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint16").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint17").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint18").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint19").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint20").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint21").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint22").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint23").transform);
+        spawnPoints.Add(GameObject.Find("SpawnPoint24").transform);
         switch (waveType)
         {
             case 1:
@@ -39,12 +63,18 @@ public class WaveSpawn : MonoBehaviour {
                 break;
             case 2:
                 enemies.Add(knightSheildSword);
+                enemies.Add(knightLance);
+                enemies.Add(knightCrossbow);
                 break;
             case 3:
-                enemies.Add(elf);
+                enemies.Add(elfMage);
+                enemies.Add(elfSage);
+                enemies.Add(elfBow);
                 break;
             case 4:
-                enemies.Add(orc);
+                enemies.Add(orcMelee);
+                enemies.Add(orcThrowing);
+                enemies.Add(orcBrute);
                 break;
             default:
                 enemies.Add(enemy);
