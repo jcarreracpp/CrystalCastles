0c9cf88bbae668c4408ac7f16474d0d58b021b78
bemetcalf <bryc3m3tcalf@aol.com>
2018-05-27 21:11:29 -0700

    enemy attacks and range
    -enemies now have various stopping and attack ranges

---
+++ Assets/Scripts/AttackWithinRange.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackWithinRange : MonoBehaviour {
+
+    public float range;
+    Transform player;
+    public float frequency;
+    public int damage;
+    private float timer;
+    private Player playerHealth;
+    
+
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = 0;
+        player = GameObject.Find("Player").transform;
+        playerHealth = player.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (Vector3.Distance(this.transform.position, player.transform.position) <= range && timer >= frequency)
+        {
+            playerHealth.modifyHealth(-damage);
+        }
+
+        if (timer >= frequency)
+        {
+            timer = 0;
+        }
+    }
+
+    
+}
